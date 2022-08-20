ee126544c37518d7a3e791aa24d6cac3d7678955
Abdullah Mohammed <abdullahmohammedsaudi@gmail.com>
2022-08-20 18:24:04 +0200

    Completion of index.html and team.html (need to be Responsive)

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Open bar*/
+$('#bar').click(function(){
+    $('#header div ul').addClass('active')
+});
+
+/*Close bar*/
+$('#bar-close').click(function(){
+    $('#header div ul').removeClass('active')
+});
